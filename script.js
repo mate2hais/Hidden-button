74c5f370bfd7575d4fd56d9a522f88b357e3440b
mate2hais <matei.adrian694@yahoo.com>
2025-02-12 10:41:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const buttonCount = 3;
-winnerId =  Math.ceil(Math.random() * buttonCount);
+const winnerId = Math.ceil(Math.random() * buttonCount);
 const output = document.querySelector('#randomText');
 
 function handleClick(clickedId) {
     if (Number(clickedId) === winnerId) {
-         output.textContent += "Congratulations! ";
+        output.textContent += "Congratulations! ";
     } else {
         output.textContent += "  Keep trying!";
     }   
